e9a8cb275dc241402d913e71cd5d824d13fcf0bc
John Lakra <104188144+Johnlakra@users.noreply.github.com>
2024-09-12 18:50:03 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import leaderBoardSlice from "./leaderBoard/leaderBoardSlice";
 import dropdownSlice from "./dropdown/dropdownSlice";
+import liveonstageSlice from "./liveonstage/liveonstageSlice";
 
 export default configureStore({
   reducer: {
     leaderBoardSlice,
     dropdownSlice,
-    liveonstageSlice
+    liveonstageSlice,
   },
 });
