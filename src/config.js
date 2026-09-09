d4a130dcbbd8bb9f9f568fc14c83401cc832e51e
John Lakra <johnamanlakra000@gmail.com>
2026-09-09 10:24:21 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  apiEndPoint: "https://eventbe-production-9b67.up.railway.app/",
+  apiEndPoint: "https://cydbe.softechsmartsolutions.com/",
 };
